5d719fe7760f635280484ba9e9567ed140b3baf3
robyn <robyn>
2019-07-03 13:07:12 +1000

    Minor edits to prep for part 2

--- aoc01.py
+++ aoc01.py
@@ -3,7 +3,7 @@ import re
 def show_evaluation(my_input):
     print("  I N P U T :   "+str(my_input))
     my_output = 0
-    with open('aoc01-input01.txt', 'r') as my_file:
+    with open('aoc01-input.txt', 'r') as my_file:
         for line in my_file:
             my_output += int(line)
     print("O U T P U T :   "+ str(my_output))
