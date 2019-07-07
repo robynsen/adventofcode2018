414d89435b7cd59daa764cfdc03d10c115190a0f
robyn <robyn>
2019-07-07 19:29:08 +1000

    Added solution for part 2

--- aoc01.py
+++ aoc01.py
@@ -1,12 +1,40 @@
 import re
 
-def show_evaluation(my_input):
-    print("  I N P U T :   "+str(my_input))
+def do_part1(my_input):
+    print("     P A R T    0 1")
+    print("I N I T I A L   V A L U E :   "+str(my_input))
     my_output = 0
     with open('aoc01-input.txt', 'r') as my_file:
         for line in my_file:
             my_output += int(line)
-    print("O U T P U T :   "+ str(my_output))
+    print("              O U T P U T :   "+ str(my_output))
+
+def do_part2(my_input):
+    print("     P A R T    0 2")
+    print("I N I T I A L   V A L U E :   "+str(my_input))
+    my_freq_history = {my_input}
+    my_freq = 0
+    is_found = False
+
+    with open('aoc01-input.txt', 'r') as my_file:
+        my_lines = my_file.readlines()
+
+        i = 0
+        while (i < len(my_lines)) and not is_found:
+            my_freq += int(my_lines[i])
+            if my_freq in my_freq_history:
+                # do something
+                my_output = my_freq
+                is_found = True
+            else:
+                my_freq_history.add(my_freq)
+            i = i + 1
+            if i == len(my_lines) and not is_found:
+                i = 0
+
+    print("              O U T P U T :   "+ str(my_output))
 
 my_input = 0
-show_evaluation(my_input)
+do_part1(my_input)
+
+do_part2(my_input)
