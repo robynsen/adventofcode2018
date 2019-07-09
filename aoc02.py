10630c660048b67e6661eecb9c49c70fa34fc8b8
robyn <robyn>
2019-07-09 14:12:10 +1000

    Added solution to Part 2 using zip

--- aoc02.py
+++ aoc02.py
@@ -1,3 +1,4 @@
+# Functions to support part 1
 def has_n_char(my_string: str, my_n: int) -> bool:
     my_result = False
     my_char = 'a'
@@ -6,6 +7,23 @@ def has_n_char(my_string: str, my_n: int) -> bool:
             my_result = True            
         my_char = chr(ord(my_char) + 1)
     return my_result
+
+# Functions to support Part 2
+def differs_by_one(my_string1: str, my_string2: str) -> bool:
+    num_diffs = 0
+    if len(my_string1) == len(my_string2):
+        for a, b in zip(my_string1, my_string2):
+            if a != b:
+                num_diffs += 1
+    return (num_diffs == 1)
+
+def get_common_letters(my_string1: str, my_string2: str) -> str:
+    my_output = ''
+    if len(my_string1) == len(my_string2):
+        for a, b in zip(my_string1, my_string2):
+            if a == b:
+                my_output = my_output + a
+    return my_output
 
 def solve_part1():
     print("     P A R T    0 1")
@@ -21,4 +39,26 @@ def solve_part1():
     my_output = num_doubles * num_triples            
     print("O U T P U T :   "+ str(my_output))
 
+def solve_part2():
+    print("     P A R T    0 2")
+    is_found = False
+    my_output = ''
+
+    with open('aoc02-input.txt', 'r') as my_file:
+        my_lines = my_file.readlines()
+
+        i = 0
+        while (i < len(my_lines)) and not is_found:
+            j = i + 1
+            while (j < len(my_lines)) and not is_found:
+                # do stuff
+                if differs_by_one(my_lines[i], my_lines[j]):
+                    my_output = get_common_letters(my_lines[i], my_lines[j])
+                    is_found = True
+                j = j + 1
+            i = i + 1
+    print("O U T P U T :   "+ str(my_output))        
+
 solve_part1()
+
+solve_part2()
