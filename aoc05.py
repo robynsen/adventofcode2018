ebd6f515f50ebd849726543e3745c21ad50300e6
robyn <robyn>
2019-07-16 22:46:30 +1000

    Updated solution to problem 5 part 1 to be slightly less ugly (but still ugly)

--- aoc05.py
+++ aoc05.py
@@ -1,3 +1,15 @@
+def process_reaction(my_input: str) -> str:
+    """Returns string that has adjacent same characters of opposite cases removed """
+    my_output = ''
+    i = 0
+    while i < (len(my_input)):
+        if (i == (len(my_input) - 1)) or (my_input[i].swapcase() != my_input[i+1]):
+            my_output += my_input[i]
+        else:
+            i += 1 
+        i += 1
+    return my_output
+
 if __name__ == '__main__':
 
     with open('aoc05-input.txt', 'r') as my_file:
@@ -8,13 +20,6 @@ if __name__ == '__main__':
 
         while (len(my_old_string) != len(my_string)):
             my_old_string = my_string
-            my_string = ''
-            i = 0
-            while i < (len(my_old_string)):
-                if (i == (len(my_old_string) - 1)) or (my_old_string[i].swapcase() != my_old_string[i+1]):
-                    my_string = my_string + my_old_string[i]
-                else:
-                    i += 1 
-                i += 1
+            my_string = process_reaction(my_string)
 
     print("P A R T   0 1      O U T P U T :   " + str(len(my_string)))
